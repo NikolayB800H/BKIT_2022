f5b2907085fba9fff0cd6a361fbf535d266f16d9
Nikolay Gorkunov <nichto77@gmail.com>
2022-10-04 22:09:54 +0300

    pyqt6 check

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ from lab_python_oop.circle import Circle
 from lab_python_oop.square import Square
 
 
+# Проверяем работу pyqt6:
+import sys
+from PyQt6.QtWidgets import QApplication, QPushButton
+app = QApplication(sys.argv)
+window = QPushButton("Hello, world!")
+window.show()
+app.exec()
+#
+
 def main():
     r = Rectangle("синего", 3, 2)
     c = Circle("зеленого", 5)
