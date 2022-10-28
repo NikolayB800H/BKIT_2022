a873ecbe0b72c855b3cc9346a2ad13f968be298a
Nikolay Gorkunov <nichto77@gmail.com>
2022-10-28 19:23:32 +0300

    Fixed variant

--- main.py
+++ main.py
@@ -1,106 +1,104 @@
 # Используется для сортировки
 from operator import itemgetter
  
-class Driver:
-    """Водитель"""
-    def __init__(self, id, fio, sal, park_id):
+class Computer:
+    """Компьютер"""
+    def __init__(self, id, name, cost, clas_id):
         self.id = id
-        self.fio = fio
-        self.sal = sal
-        self.park_id = park_id
+        self.name = name
+        self.cost = cost
+        self.clas_id = clas_id
  
-class Park:
-    """Автопарк"""
+class DispClass:
+    """Дисплейный класс"""
     def __init__(self, id, name):
         self.id = id
         self.name = name
  
-class DriverPark:
+class ComputerDispClass:
     """
-    'Водители автопарка' для реализации 
+    'Компьютеры дисплейного класса' для реализации 
     связи многие-ко-многим
     """
-    def __init__(self, park_id, driver_id):
-        self.park_id = park_id
-        self.driver_id = driver_id
+    def __init__(self, clas_id, comp_id):
+        self.clas_id = clas_id
+        self.comp_id = comp_id
  
-# Автопарки
-parks = [
-    Park(1, 'Автопарк "Такси"'),
-    Park(2, 'Автопарк "Драйв"'),
-    Park(3, 'Автопарк "Бумер"'),
+# Дисплейный классы
+classes = [
+    DispClass(1, 'Класс информатики'),
+    DispClass(2, 'Класс генной инженерии'),
+    DispClass(3, 'Класс 3Д моделирования'),
  
-    Park(11, 'Автопарк "Такси" (другой)'),
-    Park(22, 'Автопарк "Драйв" (другой)'),
-    Park(33, 'Автопарк "Бумер" (другой)'),
+    DispClass(11, 'Класс информатики (другой)'),
+    DispClass(22, 'Класс генной инженерии (другой)'),
+    DispClass(33, 'Класс 3Д моделирования (другой)'),
 ]
  
-# Водители
-drivers = [
-    Driver(1, 'Артамонов', 25000, 1),
-    Driver(2, 'Петров', 35000, 2),
-    Driver(3, 'Иваненко', 45000, 3),
-    Driver(4, 'Иванов', 35000, 3),
-    Driver(5, 'Иванин', 25000, 3),
+# Компьютеры
+computers = [
+    Computer(1, 'Делл_АУМ01', 125000, 1),
+    Computer(2, 'Эпл__АУМ02', 135000, 2),
+    Computer(3, 'Асус_АУМ03', 145000, 3),
+    Computer(4, 'Делл_АУМ04', 135000, 3),
+    Computer(5, 'Асус_АУМ05', 125000, 3),
 ]
  
-drivers_parks = [
-    DriverPark(1, 1),
-    DriverPark(2, 2),
-    DriverPark(3, 3),
-    DriverPark(3, 4),
-    DriverPark(3, 5),
+computers_classes = [
+    ComputerDispClass(1, 1),
+    ComputerDispClass(2, 2),
+    ComputerDispClass(3, 3),
+    ComputerDispClass(3, 4),
+    ComputerDispClass(3, 5),
  
-    DriverPark(11, 1),
-    DriverPark(22, 2),
-    DriverPark(33, 3),
-    DriverPark(33, 4),
-    DriverPark(33, 5),
+    ComputerDispClass(11, 1),
+    ComputerDispClass(22, 2),
+    ComputerDispClass(33, 3),
+    ComputerDispClass(33, 4),
+    ComputerDispClass(33, 5),
 ]
  
 def main():
-    """Основная функция"""
- 
     # Соединение данных один-ко-многим 
-    one_to_many = [(d.fio, d.sal, p.name) 
-        for p in parks 
-        for d in drivers 
-        if d.park_id == p.id]
+    one_to_many = [(comp.name, comp.cost, clas.name) 
+        for clas in classes 
+        for comp in computers 
+        if comp.clas_id == clas.id]
     
     # Соединение данных многие-ко-многим
-    many_to_many_tmp = [(p.name, dp.park_id, dp.driver_id) 
-        for p in parks 
-        for dp in drivers_parks 
-        if p.id == dp.park_id]
+    many_to_many_tmp = [(clas.name, compclas.clas_id, compclas.comp_id) 
+        for clas in classes 
+        for compclas in computers_classes 
+        if clas.id == compclas.clas_id]
     
-    many_to_many = [(d.fio, d.sal, park_name) 
-        for park_name, park_id, driver_id in many_to_many_tmp
-        for d in drivers if d.id == driver_id]
+    many_to_many = [(comp.name, comp.cost, clas_name) 
+        for clas_name, clas_id, comp_id in many_to_many_tmp
+        for comp in computers if comp.id == comp_id]
  
-    print('Задание 1')
+    print('Задание В1')
     res_11 = list(filter(lambda x: x[0].find('А') == 0, one_to_many))
     print(res_11)
     
-    print('\nЗадание 2')
+    print('\nЗадание В2')
     res_12_unsorted = []
-    # Перебираем все автопарки
-    for p in parks:
-        # Список водителей автопарка
-        p_drivers = list(filter(lambda i: i[2] == p.name, one_to_many))
-        # Если отдел не пустой        
-        if len(p_drivers) > 0:
-            # Зарплаты водителей автопарка
-            p_sals = list(map(lambda x: x[1], p_drivers))
-            # Минимальная зарплата водителей автопарка
-            p_sals_min = min(p_sals)
-            res_12_unsorted.append((p.name, p_sals_min))
+    # Перебираем все дисплейные классы
+    for clas in classes:
+        # Список компьютеров дисплейного класса
+        clas_comps = list(filter(lambda i: i[2] == clas.name, one_to_many))
+        # Если дисплейный класс не пустой        
+        if len(clas_comps) > 0:
+            # Стоимости компьютеров дисплейного класса
+            clas_costs = list(map(lambda x: x[1], clas_comps))
+            # Минимальная стоимость среди компьютеров дисплейного класса
+            clas_costs_min = min(clas_costs)
+            res_12_unsorted.append((clas.name, clas_costs_min))
  
-    # Сортировка по минимальной зарплате
+    # Сортировка по минимальной стоимости
     res_12 = sorted(res_12_unsorted, key = itemgetter(1))
     print(res_12)
  
-    print('\nЗадание 3')
-    # Сортировка по Автопарку
+    print('\nЗадание В3')
+    # Сортировка по дисплейному классу
     res_13 = sorted(many_to_many, key = itemgetter(2))
     print(res_13)
  
